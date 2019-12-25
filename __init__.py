bd7efb503a3a8d9afd425ce628fae8b84fbd6bc7
Lateasusual <christopherclyne@rocketmail.com>
2019-12-25 00:55:59 +0000

    Added:  - Overwriting of local groups with matching name  - Console logger of imports and overwrites  - Fail-safes for opening source files with auto-load enabled

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "node group importer",
     "description": "Auto-import node groups from specified library file(s)",
     "author": "Lateasusual",
-    "version": (0, 0, 3),
+    "version": (1, 0, 0),
     "blender": (2, 80, 1),
     "location": "Node Editor",
     "warning": '',
@@ -55,7 +55,6 @@ class NOD_PT_NodeGroupImporterPanel(bpy.types.Panel):
         op.overwrite = True
 
 
-
 class NOD_OT_update_groups(bpy.types.Operator):
     """ Updates node groups linked using the importer addon """
     bl_label = "Reload Auto-Imported Node Groups"
@@ -95,18 +94,79 @@ def import_groups_from_library(path_arg, relative=True, overwrite=False):
     else:
         path = path_arg
 
+    # Skip importing this file if it's the currently open one
+    fp = bpy.data.filepath
+    if fp is not None and fp != '' and os.path.samefile(fp, path):
+        print("Skipped import of node groups from active file", fp)
+        return
+
+    if overwrite:
+        names, overrides = import_override(path)
+        if len(names):
+            print("Node groups loaded from" + (" included" if relative else ""), os.path.abspath(path), ":",
+                  names)
+        if len(overrides):
+            print("Node groups overwritten from" + (" included" if relative else ""), os.path.abspath(path), ":",
+                  overrides)
+    else:
+        names = import_keep(path, relative=relative)
+        if len(names):
+            print("Node groups loaded from" + (" included" if relative else ""), os.path.abspath(path), ":",
+                  names)
+
+
+def import_override(path_abs):
+    D = bpy.data
+
+    def appender(name):
+        return "_DELETE_ME_" + name
+
+    current_file = bpy.data.node_groups
+    with bpy.data.libraries.load(path_abs) as (data_from, data_to):
+        # Pass 1, non-overriden groups
+        data_to.node_groups = [x for x in data_from.node_groups if x not in D.node_groups]
+        names = [x for x in data_from.node_groups if x not in D.node_groups]
+        override_names = set([x for x in data_from.node_groups if x in D.node_groups])
+        ret_names = [x for x in data_from.node_groups if x in D.node_groups]
+
+    mapping = dict([(appender(g.name), g.name) for g in D.node_groups if g.name in override_names])
+
+    # "Backup" groups
+    for g in D.node_groups:
+        if g.name in override_names:
+            g.name = appender(g.name)
+
+    with D.libraries.load(path_abs) as (data_from, data_to):
+        data_to.node_groups = [x for x in data_from.node_groups if x in override_names]
+
+    for g in D.node_groups:
+        if g.name in override_names:
+
+            old = D.node_groups[appender(g.name)]
+            for mat in bpy.data.materials:
+
+                for node in mat.node_tree.nodes:
+                    if node.type == 'GROUP':
+                        if node.node_tree.name in mapping.keys():
+                            node.node_tree = g
+
+            D.node_groups.remove(old)
+
+    return names, ret_names
+
+
+def import_keep(path, relative=True):
+    names = []
     # Load only node groups from library file as new blocks
     with bpy.data.libraries.load(path) as (data_from, data_to):
         data_to.node_groups = [x for x in data_from.node_groups
-                               if overwrite or x not in bpy.data.node_groups]
-
-        print("Node groups loaded from" + (" included" if relative else ""), path_arg, ":", data_to.node_groups)
-
+                               if x not in bpy.data.node_groups]
+        names.append(data_to.node_groups)
         # Keep our groups please
         for datablock in data_to.node_groups:
             if datablock in bpy.data.node_groups:
                 bpy.data.node_groups[datablock].use_fake_user = True
-
+    return names
 
 @persistent
 def load_handler(dummy):
